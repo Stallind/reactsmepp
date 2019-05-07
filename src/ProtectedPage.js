d30a8df778a6716355f70676774254bc0d1800d8
Stefan Lindahl <stefan.lindahl87@gmail.com>
2019-05-07 13:32:24 +0200

    Add axios/fetch test to protectedpage.js

--- src/ProtectedPage.js
+++ src/ProtectedPage.js
@@ -3,25 +3,65 @@ import axios from "axios";
 
 let apiBaseUrl = "https://localhost:44339/api/courses";
 
-let result = axios.get(apiBaseUrl)
-    .then( response => {
+// axios.get(apiBaseUrl)
+//     .then( response => {
+//     console.log(response.data);
+//         return response.data;
+//     });
 
-        return response.data;
-    });
+class ProtectedPage extends React.Component {
+    state = {
+        courses: [],
+        isLoading: true,
+        errors: null
+    };
 
-console.log(result);
+    componentDidMount()  {
+        axios.get(apiBaseUrl)
+            // Once we get a response, we'll map the API endpoints to our props
+            .then(response =>
+                response.data.results.map(courses => ({
+                    name: `${courses.name}`,
+                    points: `${courses.points}`
+                }))
+            )
+            .then(courses => {
+                this.setState({
+                    courses,
+                    isLoading: false
+                });
+            })
+            .catch(error => this.setState({ error, isLoading: false }));
+        console.log(this.state.courses);
+    }
 
-const ProtectedPage = () => {
 
-    return (
+
+    render() {
+        return (
             <React.Fragment>
                 <h1>
-                    Your courses are {result.length}
+                    Your courses are {}
                 </h1>
             </React.Fragment>
+        );
+    }
+}
 
-    );
-};
 
+
+
+
+// const ProtectedPage = () => {
+//
+//     return (
+//             <React.Fragment>
+//                 <h1>
+//                     Your courses are {}
+//                 </h1>
+//             </React.Fragment>
+//
+//     );
+// };
 
 export default ProtectedPage;
