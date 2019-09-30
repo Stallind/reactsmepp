2e545ff69ac63e29d9b3c71efa1e34bc2bcee3f1
Stalli <Stefan.Lindahl87@gmail.com>
2019-09-30 15:18:41 +0200

    Updates

---
+++ precache-manifest.bc868f9a04a3689ec4aba28a44208358.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4794499a5612ef5033ae2ad6cf4806ef",
+    "url": "/static/media/grades-logo.4794499a.jpg"
+  },
+  {
+    "revision": "7f9d08badd08256cfa4af4efec026bae",
+    "url": "/static/media/User-logo.7f9d08ba.jpg"
+  },
+  {
+    "revision": "e8798acb15346c6eaeecb95da5459c9d",
+    "url": "/static/media/Reg-logo.e8798acb.jpg"
+  },
+  {
+    "revision": "1217350a641adb86459b577f296f2200",
+    "url": "/static/media/BirdBlue.1217350a.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "24716268868706dde579",
+    "url": "/static/js/main.6152af89.chunk.js"
+  },
+  {
+    "revision": "00a18d542ba4de731f69",
+    "url": "/static/js/2.a65a945d.chunk.js"
+  },
+  {
+    "revision": "24716268868706dde579",
+    "url": "/static/css/main.ec6ddb81.chunk.css"
+  },
+  {
+    "revision": "6f1a4004cf92a52100217594a361894e",
+    "url": "/index.html"
+  }
+];
